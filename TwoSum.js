9cd084405f258d2463806a9dba9f366a97848cb0
Pablo Aucapiña <soluciones.pera@gmail.com>
2025-05-14 18:25:41 -0500

    Solution with map from other

--- TwoSum.js
+++ TwoSum.js
@@ -4,22 +4,24 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-  let i = 0;
-  for (i = 0; i < nums.length; i++) {
-    let sum = nums[i] + nums[i + 1];
-    if (sum === target) break;
+  // Dictionary used to store the index of each number
+  let pairIndex = {};
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i];
+    // If the difference between the current number and the target is in the dictionary we save the index
+    if (target - num in pairIndex) return [pairIndex[target - num], i];
+    pairIndex[num] = i;
   }
-  return [i, i + 1];
 };
 
-let result = twoSum([2, 7, 11, 15], 9);
-console.log(result);
+// let result = twoSum([2, 7, 11, 15], 9);
+// console.log(result);
 
-result = twoSum([3, 2, 4], 6);
-console.log(result);
+// result = twoSum([3, 2, 4], 6);
+// console.log(result);
 
-result = twoSum([3, 3], 6);
-console.log(result);
+// result = twoSum([3, 3], 6);
+// console.log(result);
 
 result = twoSum([3, 2, 3], 6);
 console.log(result);
