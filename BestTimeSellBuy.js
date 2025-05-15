8a57e54ac2ef7bf62137da49685a9bc093397c8b
Pablo Aucapiña <soluciones.pera@gmail.com>
2025-05-15 17:26:34 -0500

    Base code

---
+++ BestTimeSellBuy.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+    let profit = [];
+    for (let i = 0; i < prices.length; i++) {
+        let calc = [];
+        for (let j = i + 1; j < prices.length; j++) {
+            calc.push(prices[j] - prices[i]);
+        }
+        let sorted = calc.toSorted((a, b) => b - a);
+        profit.push(sorted[0]);
+    }
+    const max = profit.toSorted((a, b) => b - a);
+    return max[0] > 0 ? max[0] : 0;
+
+};
