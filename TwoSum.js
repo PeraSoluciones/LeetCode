6d56cf8f0179c380a1ea01ab2c2d5209a2de5514
Pablo Aucapiña <soluciones.pera@gmail.com>
2025-05-14 22:01:05 -0500

    Coments added

--- TwoSum.js
+++ TwoSum.js
@@ -4,12 +4,16 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
+  // Hashmap used
   let map = new Map();
   for (let i = 0; i < nums.length; i++) {
+    // Use formula complement = target - nums[i]
     let complement = target - nums[i];
+    // If complement already exists return position array
     if (map.has(complement)) {
       return [map.get(complement), i];
     }
+    // else add to map
     map.set(nums[i], i);
   }
   return [];
