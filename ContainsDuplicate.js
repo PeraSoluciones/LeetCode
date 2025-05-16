5847304340087bbbfcd889967404b523a9371b6b
Pablo Aucapiña <soluciones.pera@gmail.com>
2025-05-16 16:27:07 -0500

    Contiene duplicados mi solución

--- ContainsDuplicate.js
+++ ContainsDuplicate.js
@@ -3,5 +3,18 @@
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
-    
+    let result = false;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                result = true;
+                nums.splice(j, 1);
+            }
+        }
+    }
+    return result;
 };
+
+console.log(containsDuplicate([1, 2, 3, 1]));
+console.log(containsDuplicate([1, 2, 3, 4]));
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
