6b771a64f97f5d8aaf188aae14ed21e727b79f2c
Pablo Aucapiña <soluciones.pera@gmail.com>
2025-05-16 17:08:28 -0500

    Contiene duplicados solución del usuario niits de leetcode

--- ContainsDuplicate.js
+++ ContainsDuplicate.js
@@ -3,5 +3,9 @@
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
-    
+    nums.sort((a, b) => b - a);
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] === nums[i - 1]) return true;
+    }
+    return false;
 };
