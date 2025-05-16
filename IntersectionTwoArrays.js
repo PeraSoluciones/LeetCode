20c9d87f0e5332f0527cc3568f2307385a358b13
Pablo Aucapiña <soluciones.pera@gmail.com>
2025-05-16 17:57:24 -0500

    Intersección de dos arrays mi solución

--- IntersectionTwoArrays.js
+++ IntersectionTwoArrays.js
@@ -4,5 +4,15 @@
  * @return {number[]}
  */
 var intersection = function (nums1, nums2) {
-    
+    const output = new Array();
+    for (let i = 0; i < nums2.length; i++) {
+        for (let j = 0; j < nums1.length; j++) {
+            if (nums2[i] === nums1[j] && !output.includes(nums2[i]))
+                output.push(nums2[i]);
+        }
+    }
+    return output;
 };
+
+console.log(intersection([1, 2, 2, 1], [2, 2]));
+console.log(intersection([4, 9, 5], [9, 4, 9, 8, 4]));
