eae277c181a7db798df4dca8cf6e31f84772c492
Pablo Aucapiña <soluciones.pera@gmail.com>
2025-05-15 21:23:35 -0500

    Anagram mi solución

--- anagram.js
+++ anagram.js
@@ -3,6 +3,18 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
-    
+var isAnagram = function (s, t) {
+    let _s = s.split('').sort();
+    let _t = t.split('').sort();
+
+    if (_s.length !== _t.length) return false;
+
+    for (let i = 0; i < _s.length; i++) {
+        if (_s[i] !== _t[i]) return false;
+    }
+
+    return true;
 };
+
+// console.log(isAnagram('anagram', 'nagaram'));
+console.log(isAnagram('a', 'ab'));
