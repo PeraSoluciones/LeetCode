d3f7a719be373f836e85b128b40479cbd82bdb3e
Pablo Aucapiña <soluciones.pera@gmail.com>
2025-05-17 00:24:46 -0500

    Dos anagramas solución del usuario de leetcode niits

--- TwoAnagrams.js
+++ TwoAnagrams.js
@@ -3,5 +3,19 @@
  * @return {string[][]}
  */
 var groupAnagrams = function (strs) {
-    
+    let ans = {};
+
+    for (let s of strs) {
+        let key = s.split('').sort().join('');
+        if (!ans[key]) {
+            ans[key] = [];
+        }
+        ans[key].push(s);
+    }
+
+    return Object.values(ans);
 };
+
+console.log(groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']));
+console.log(groupAnagrams(['']));
+console.log(groupAnagrams(['a']));
