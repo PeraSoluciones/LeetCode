19b75e4b66ac666d8aa917a081132953f38c5247
Pablo Aucapiña <soluciones.pera@gmail.com>
2025-05-17 00:05:41 -0500

    Dos anagramas mi solución

--- TwoAnagrams.js
+++ TwoAnagrams.js
@@ -3,5 +3,19 @@
  * @return {string[][]}
  */
 var groupAnagrams = function (strs) {
-    
+    const set = new Set(
+        strs.map((s) => {
+            return { [s]: s.split('').sort().join('') };
+        })
+    );
+    const resultSet = {};
+    for (const entries of set) {
+        const [array] = Object.entries(entries);
+        if (resultSet[array[1]])
+            resultSet[array[1]] = [...resultSet[array[1]], array[0]];
+        else resultSet[array[1]] = array[0];
+    }
+    return Object.values(resultSet);
 };
+
+console.log(groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']));
