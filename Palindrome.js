0711cd5324d59338416276f0c032d5ab2952cf1e
Pablo Aucapiña <soluciones.pera@gmail.com>
2025-05-17 19:49:52 -0500

    Mi solución

--- Palindrome.js
+++ Palindrome.js
@@ -2,6 +2,19 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
-    
+var isPalindrome = function (s) {
+    if (s.trim().length === 0) return true;
+    let regex = /[a-z\d]/gi;
+    let normalized = s.toLowerCase().match(regex);
+    if (!normalized) return true;
+    let reverse = [...normalized].reverse().join('');
+
+    return normalized.join('') === reverse;
 };
+
+console.log(isPalindrome('A man, a plan, a canal: Panama'));
+console.log(isPalindrome('race a car'));
+console.log(isPalindrome(' '));
+console.log(isPalindrome('.'));
+console.log(isPalindrome('.,'));
+console.log(isPalindrome('0P'));
