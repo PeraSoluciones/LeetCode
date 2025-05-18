e02d4f4b9f33a43c737086dc8feca75cd342a8b4
Pablo Aucapiña <soluciones.pera@gmail.com>
2025-05-17 23:02:20 -0500

    Solución del usuario de leetcode niits

--- MoveZerores.js
+++ MoveZerores.js
@@ -3,5 +3,19 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function (nums) {
-    
+    let left = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] !== 0) {
+            [nums[right], nums[left]] = [nums[left], nums[right]];
+            left++;
+        }
+    }
+
+    return nums;
 };
+
+console.log(moveZeroes([0, 1, 0, 3, 12]));
+console.log(moveZeroes([0]));
+console.log(moveZeroes([0, 0, 1]));
+console.log(moveZeroes([1, 0, 0]));
