a259f24377cf2c4382d81555de29d5604a967487
Pablo Aucapiña <soluciones.pera@gmail.com>
2025-05-20 14:16:50 -0500

    Mi solución

--- TwoSumII.js
+++ TwoSumII.js
@@ -4,5 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function (numbers, target) {
-    
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] === target) return [i + 1, j + 1];
+        }
+    }
+    return 0;
 };
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([2, 3, 4], 6));
+console.log(twoSum([-1, 0], -1));
