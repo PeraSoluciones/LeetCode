682ce4bb6a617bfdaf243049cbb2a92edf58b446
Pablo Aucapiña <soluciones.pera@gmail.com>
2025-05-23 23:12:08 -0500

    Mi solución

--- ValidParentheses.js
+++ ValidParentheses.js
@@ -3,5 +3,21 @@
  * @return {boolean}
  */
 var isValid = function (s) {
-    
+    let parentheses = { '(': ')', '[': ']', '{': '}' };
+    let array = s.split('');
+    let i = 0;
+    for (let j = 1; j < array.length; j++) {
+        if (parentheses[array[i]] === array[j]) {
+            array.splice(i, 2);
+            i--;
+            j -= 2;
+        } else i++;
+    }
+
+    return array.length < 1;
 };
+
+console.log(isValid('()'));
+console.log(isValid('()[]{}'));
+console.log(isValid('(]'));
+console.log(isValid('({[]})'));
