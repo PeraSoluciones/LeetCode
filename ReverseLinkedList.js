044299cc43016d0f7c929182b06dbd0d5c6829bd
Pablo Aucapiña <soluciones.pera@gmail.com>
2025-05-29 22:13:10 -0500

    Solución adaptada

--- ReverseLinkedList.js
+++ ReverseLinkedList.js
@@ -10,5 +10,16 @@
  * @return {ListNode}
  */
 var reverseList = function (head) {
-    
+    let node = null;
+
+    while (head) {
+        [head.next, node, head] = [node, head, head.next];
+        // const temp = head.next;
+        // head.next = node;
+        // node = head;
+        // head = temp;
+    }
+    return node;
 };
+
+console.log(reverseList([1, 2, 3, 4, 5]));
