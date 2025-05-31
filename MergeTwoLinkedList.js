bf7b1bf922164658fffefcc5cf8c407d185d0a54
Pablo Aucapiña <soluciones.pera@gmail.com>
2025-05-30 21:51:57 -0500

    Solución recursiva

--- MergeTwoLinkedList.js
+++ MergeTwoLinkedList.js
@@ -11,5 +11,24 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function (list1, list2) {
-    
+    if (list1 === null) {
+        return list2;
+    }
+    if (list2 === null) {
+        return list1;
+    }
+    if (list1.val < list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
+    }
 };
+
+console.log(
+    mergeTwoLists(
+        { val: 5, next: null },
+        { val: 1, next: { val: 2, next: { val: 4, next: null } } }
+    )
+);
