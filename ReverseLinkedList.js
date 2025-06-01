52cc2955c6319249158fff243c376a0d26f53c39
Pablo Aucapiña <soluciones.pera@gmail.com>
2025-06-01 16:33:06 -0500

    Solución recursiva

--- ReverseLinkedList.js
+++ ReverseLinkedList.js
@@ -10,5 +10,32 @@
  * @return {ListNode}
  */
 var reverseList = function (head) {
-    
+    let reversed = head;
+    let curr = null;
+    if (head.next) reversed = reverseList(head.next);
+    else return head;
+    reversed.next = curr;
+    head.next = null;
+    return head;
 };
+
+console.log(
+    reverseList({
+        val: 1,
+        next: {
+            val: 2,
+            next: {
+                val: 3,
+                next: {
+                    val: 4,
+                    next: {
+                        val: 5,
+                        next: { val: 6, next: { val: 7, next: null } },
+                    },
+                },
+            },
+        },
+    })
+);
+
+// console.log(reverseList({ val: 0, next: null }));
