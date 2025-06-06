27e20948de390365ae6eb4bc59dec73c81a4c9f4
Pablo Aucapiña <soluciones.pera@gmail.com>
2025-06-06 18:00:15 -0500

    Mi solución

--- BinarySearch.js
+++ BinarySearch.js
@@ -4,5 +4,12 @@
  * @return {number}
  */
 var search = function (nums, target) {
-    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) return i;
+    }
+    return -1;
 };
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9));
+console.log(search([-1, 0, 3, 5, 9, 12], 2));
+console.log(search([-1, 0, 3, 5, 9, 12], -1));
