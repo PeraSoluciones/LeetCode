ceb4681bc281a224198ebfd89f7ef07d8ec3f092
Pablo Aucapiña <soluciones.pera@gmail.com>
2025-06-11 11:07:44 -0500

    Solución del usuario de leetcode niits

--- FindPeakElement.js
+++ FindPeakElement.js
@@ -3,5 +3,15 @@
  * @return {number}
  */
 var findPeakElement = function (nums) {
-
+    let left = 0;
+    let right = nums.length - 1;
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] < nums[mid + 1]) left = mid + 1;
+        else right = mid;
+    }
+    return left;
 };
+
+console.log(findPeakElement([1, 2, 3, 1]));
+console.log(findPeakElement([1, 2, 1, 3, 5, 6, 4]));
