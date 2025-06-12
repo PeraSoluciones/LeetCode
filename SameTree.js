d9725708f6d69ddc89dbc8bb365421814411d7c8
Pablo Aucapiña <soluciones.pera@gmail.com>
2025-06-12 17:49:20 -0500

    My solution

--- SameTree.js
+++ SameTree.js
@@ -12,5 +12,11 @@
  * @return {boolean}
  */
 var isSameTree = function (p, q) {
-    
+    if (!p && !q) return true;
+    if (!p || !q || p.val !== q.val) return false;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
+
+console.log(isSameTree([1, 2, 3], [1, 2, 3]));
+console.log(isSameTree([1, 2], [1, null, 2]));
+console.log(isSameTree([1, 2, 1], [1, 1, 2]));
