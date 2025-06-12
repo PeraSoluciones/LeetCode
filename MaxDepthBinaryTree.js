08cbb06fbe5547a9c7fb6a07dce0633a15493842
Pablo Aucapiña <soluciones.pera@gmail.com>
2025-06-11 22:06:05 -0500

    Solution with help of windsurf

--- MaxDepthBinaryTree.js
+++ MaxDepthBinaryTree.js
@@ -11,5 +11,8 @@
  * @return {number}
  */
 var maxDepth = function (root) {
-    
+    if (!root) return 0;
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 };
+
+console.log(maxDepth([3, 9, 20, null, null, 15, 7]));
